fddff08c76f0406aa03cc225a8fe66adb8cd76a8
kkntzw <64656589+kkntzw@users.noreply.github.com>
2021-08-01 20:23:08 +0900

    #1 環境変数を定数化するモジュールを追加
    環境変数を定数として呼び出すため constant モジュールを追加した。

---
+++ src/constant.ts
@@ -0,0 +1,5 @@
+// 環境変数から定数を設定する。
+export const BOT_TOKEN = process.env['BOT_TOKEN']; // BOTのトークン
+export const APPLICATION_ID = process.env['APPLICATION_ID']; // BOTのID
+export const GUILD_ID = process.env['GUILD_ID']; // テストサーバのID
+export const ADMIN_ID = process.env['ADMIN_ID']; // 管理者のID
