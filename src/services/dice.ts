5dccf983d51ec8086691d7c67d0e098a606a30fa
kkntzw <64656589+kkntzw@users.noreply.github.com>
2021-08-02 22:35:57 +0900

    #3 ダイスロールを行うサービスを追加
    ダイスロールを行い、実測値と期待値から結果を判定するサービスを追加した。

---
+++ src/services/dice.ts
@@ -0,0 +1,101 @@
+import { Judgement } from '../enums/judgement';
+
+/**
+ * ダイスに関するサービス。
+ *
+ * @author kkntzw
+ */
+export namespace DiceService {
+
+  /**
+   * ダイスロールを行う。
+   *
+   * @param times 回数
+   * @param faces 面
+   * @returns 出目の合計値
+   */
+  export const roll = (times: number, faces: number) => {
+    // 回数が自然数でなければ例外を投げる。
+    if (!Number.isInteger(times) || times < 1) {
+      throw new RangeError();
+    }
+
+    // 面が自然数でなければ例外を投げる。
+    if (!Number.isInteger(faces) || faces < 1) {
+      throw new RangeError();
+    }
+
+    // 指定した回数ダイスロールを行い、出目の合計値を返却する。
+    let total = 0;
+    for (let i = 0; i < times; ++i) {
+      total += Math.ceil(Math.random() * faces);
+    }
+    return total;
+  };
+
+  /**
+   * ダイスロールの結果が成功か判定する。
+   *
+   * 実測値が期待値以下の場合、成功と判定する。
+   *
+   * @param total 実測値
+   * @param target 期待値
+   * @returns 成功/失敗
+   */
+  export const isSuccessful = (total: number, target: number): Judgement => {
+    return total <= target ? Judgement.SUCCESS : Judgement.FAILURE;
+  };
+
+  /**
+   * ダイスロールの結果が決定的成功か判定する。
+   *
+   * 実測値が判定上限以下の場合、決定的成功と判定する。
+   *
+   * @param total 実測値
+   * @param limit 判定上限
+   * @returns 決定的成功/成功
+   */
+  export const isCritical = (total: number, limit: number): Judgement => {
+    return total <= limit ? Judgement.CRITICAL : Judgement.SUCCESS;
+  };
+
+  /**
+   * ダイスロールの結果が致命的失敗か判定する。
+   *
+   * 実測値が判定下限以上の場合、致命的失敗と判定する。
+   *
+   * @param total 実測値
+   * @param limit 判定下限
+   * @returns 致命的失敗/失敗
+   */
+  export const isFumble = (total: number, limit: number): Judgement => {
+    return total >= limit ? Judgement.FUMBLE : Judgement.FAILURE;
+  };
+
+  /**
+   * ダイスロールの結果を判定する。
+   *
+   * @param total 実測値
+   * @param target 期待値
+   * @param criticalLimit 決定的成功の判定上限
+   * @param fumbleLimit 致命的失敗の判定下限
+   * @returns 成功/失敗/決定的成功/致命的失敗
+   */
+  export const judge = (total: number, target: number, criticalLimit?: number, fumbleLimit?: number) => {
+    const judgement = isSuccessful(total, target);
+
+    // 判定上限または判定下限が設定されていない場合はその場で結果を返却する。
+    if (criticalLimit === undefined || fumbleLimit === undefined) {
+      return judgement;
+    }
+
+    // 成功の場合は決定的成功の判定を返却する。
+    if (judgement === Judgement.SUCCESS) {
+      return isCritical(total, criticalLimit);
+    }
+
+    // 失敗の場合は致命的失敗の判定を返却する。
+    return isFumble(total, fumbleLimit);
+  };
+
+}
---
+++ test/services/dice.test.ts
@@ -0,0 +1,119 @@
+import { Judgement } from '../../src/enums/judgement';
+import { DiceService } from '../../src/services/dice';
+
+test(
+  'メソッド roll() は指定した面のダイスを指定した回数振った出目の合計値を返却する。',
+  () => {
+    expect(DiceService.roll(10, 1)).toBe(10);
+  }
+);
+
+test(
+  'メソッド roll() の戻り値の最小値は times * 1 になる。',
+  () => {
+    for (let i = 0; i < 100; ++i) {
+      expect(DiceService.roll(10, 6)).toBeGreaterThanOrEqual(10);
+    }
+  }
+);
+
+test(
+  'メソッド roll() の戻り値の最大値は times * faces になる。',
+  () => {
+    for (let i = 0; i < 100; ++i) {
+      expect(DiceService.roll(10, 6)).toBeLessThanOrEqual(60);
+    }
+  }
+);
+
+test(
+  'メソッド roll() の戻り値は整数になる。',
+  () => {
+    for (let i = 0; i < 100; ++i) {
+      expect(Number.isInteger(DiceService.roll(10, 6))).toBe(true);
+    }
+  }
+);
+
+test(
+  'メソッド roll() の引数 times に 自然数でない値を指定した場合は例外が発生する。',
+  () => {
+    expect(() => { DiceService.roll(-1, 1) }).toThrow(RangeError);
+    expect(() => { DiceService.roll(0, 1) }).toThrow(RangeError);
+    expect(() => { DiceService.roll(1.5, 1) }).toThrow(RangeError);
+  }
+);
+
+test(
+  'メソッド roll() の引数 faces に 自然数でない値を指定した場合は例外が発生する。',
+  () => {
+    expect(() => { DiceService.roll(1, -1) }).toThrow(RangeError);
+    expect(() => { DiceService.roll(1, 0) }).toThrow(RangeError);
+    expect(() => { DiceService.roll(1, 1.5) }).toThrow(RangeError);
+  }
+);
+
+test(
+  'メソッド isSuccessful() は実測値（出目の合計値）が期待値以下の場合、成功と判定する。',
+  () => {
+    expect(DiceService.isSuccessful(49, 50)).toBe(Judgement.SUCCESS);
+    expect(DiceService.isSuccessful(50, 50)).toBe(Judgement.SUCCESS);
+  }
+);
+
+test(
+  'メソッド isSuccessful() は実測値（出目の合計値）が期待値より大きい場合、失敗と判定する。',
+  () => {
+    expect(DiceService.isSuccessful(51, 50)).toBe(Judgement.FAILURE);
+  }
+);
+
+test(
+  'メソッド isCritical() は実測値が判定上限以下の場合、決定的成功と判定する。',
+  () => {
+    expect(DiceService.isCritical(4, 5)).toBe(Judgement.CRITICAL);
+    expect(DiceService.isCritical(5, 5)).toBe(Judgement.CRITICAL);
+  }
+);
+
+test(
+  'メソッド isCritical() は実測値が判定上限より大きい場合、成功と判定する。',
+  () => {
+    expect(DiceService.isCritical(6, 5)).toBe(Judgement.SUCCESS);
+  }
+);
+
+test(
+  'メソッド isFumble() は実測値が判定下限以上の場合、致命的失敗と判定する。',
+  () => {
+    expect(DiceService.isFumble(97, 96)).toBe(Judgement.FUMBLE);
+    expect(DiceService.isFumble(96, 96)).toBe(Judgement.FUMBLE);
+  }
+);
+
+test(
+  'メソッド isFumble() は実測値が判定下限より小さい場合、失敗と判定する。',
+  () => {
+    expect(DiceService.isFumble(95, 96)).toBe(Judgement.FAILURE);
+  }
+);
+
+test(
+  'メソッド judge() はダイスロールの結果を判定する。',
+  () => {
+    expect(DiceService.judge(49,  50, 5, 96)).toBe(Judgement.SUCCESS);
+    expect(DiceService.judge(51,  50, 5, 96)).toBe(Judgement.FAILURE);
+    expect(DiceService.judge(1,   50, 5, 96)).toBe(Judgement.CRITICAL);
+    expect(DiceService.judge(100, 50, 5, 96)).toBe(Judgement.FUMBLE);
+  }
+);
+
+test(
+  'メソッド judge() は判定上限または判定下限を省略した場合、クリファン判定を行わない。',
+  () => {
+    expect(DiceService.judge(1,   50)).toBe(Judgement.SUCCESS);
+    expect(DiceService.judge(100, 50)).toBe(Judgement.FAILURE);
+    expect(DiceService.judge(1,   50, 5)).toBe(Judgement.SUCCESS);
+    expect(DiceService.judge(100, 50, undefined, 96)).toBe(Judgement.FAILURE);
+  }
+);
