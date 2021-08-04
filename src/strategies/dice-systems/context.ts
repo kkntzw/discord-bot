3030bc0a51aa840727b66bd8ce7abac3d59fee0c
kkntzw <64656589+kkntzw@users.noreply.github.com>
2021-08-04 23:33:20 +0900

    #5 ダイスシステムにRESを追加
    ダイスシステムにクリティカル判定上限1, ファンブル判定下限100で対抗ロールを行う RES を追加した。

--- src/strategies/dice-systems/context.ts
+++ src/strategies/dice-systems/context.ts
@@ -3,6 +3,7 @@ import { CCStrategy } from './cc';
 import { CCBStrategy } from './ccb';
 import { ChoiceStrategy } from './choice';
 import { DStrategy } from './d';
+import { RESStrategy } from './res';
 import { RESBStrategy } from './resb';
 
 // /d の <command> に指定するストラテジを定義する。
@@ -11,5 +12,6 @@ export const DICE_SYSTEMS: DiceSystemStrategy[] = [
   new CCBStrategy(),
   new ChoiceStrategy(),
   new DStrategy(),
+  new RESStrategy(),
   new RESBStrategy(),
 ];
