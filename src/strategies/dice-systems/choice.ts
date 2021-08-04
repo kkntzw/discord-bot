aac2dba5b15f59bdb8f02bd412a0e9e4ee7fd9ae
kkntzw <64656589+kkntzw@users.noreply.github.com>
2021-08-04 19:19:39 +0900

    #5 ダイスシステムにchoiceを追加
    与えた候補の中から無作為に選択した結果を返却する choice をダイスシステムに追加した。

---
+++ src/strategies/dice-systems/choice.ts
@@ -0,0 +1,50 @@
+import { MessageService } from '../../services/message';
+import { DiceService } from '../../services/dice';
+import { DiceSystemStrategy } from '.';
+
+export class ChoiceStrategy implements DiceSystemStrategy {
+
+  name = 'choice';
+
+  /**
+   * ダイスシステムの正規表現。
+   *
+   * args は選択肢を表し、1文字以上の文字列をとる。
+   */
+  regexp = /^choice\((?<args>.+)\)$/i;
+
+  evaluate(command: string, comment?: string) {
+    // コマンドの形式が不正な場合は異常系メッセージを返却して終了。
+    const groups = this.regexp.exec(command)?.groups;
+    if (groups === undefined) {
+      return MessageService.syntaxErrorMessage('コマンドの形式がおかしいよ😳');
+    }
+
+    // 選択肢を設定する。
+    const args = groups['args']?.replace(/\s+/g, '').split(',');
+
+    // 選択肢の形式が不正な場合は異常系メッセージを返却して終了。
+    if (args === undefined || args.indexOf('') > -1) {
+      return MessageService.syntaxErrorMessage('選択肢の形式がおかしいよ😳');
+    }
+
+    // 面を設定する。
+    const faces = args.length;
+
+    // ダイスロールを行う。
+    const index = DiceService.roll(1, faces) - 1;
+
+    // インデックスが不正な場合は異常系メッセージを返却して終了。
+    const result = args[index];
+    if (result === undefined) {
+      return MessageService.notFoundMessage('選択肢が見つからなかったよ😳');
+    }
+
+    // 正常系メッセージを返却して終了。
+    return MessageService.message(
+      0x888888,
+      DiceService.toTitle(command, comment),
+      DiceService.toString(command, result)
+    );
+  }
+}
--- src/strategies/dice-systems/context.ts
+++ src/strategies/dice-systems/context.ts
@@ -1,11 +1,13 @@
 import { DiceSystemStrategy } from '.';
 import { CCStrategy } from './cc';
 import { CCBStrategy } from './ccb';
+import { ChoiceStrategy } from './choice';
 import { DStrategy } from './d';
 
 // /d の <command> に指定するストラテジを定義する。
 export const DICE_SYSTEMS: DiceSystemStrategy[] = [
   new CCStrategy(),
   new CCBStrategy(),
+  new ChoiceStrategy(),
   new DStrategy(),
 ];
